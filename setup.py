e9f1b5449e1eec6993d410109f38e13dbf95f296
Oleg Silkin <o.silkin98@gmail.com>
2018-08-18 22:47:03 -0500

    added class that inherits from build_py to initialize keys.py on setup

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from distutils.core import setup
+from distutils.command.build_py import build_py
+import os
+from os.path import exists
+
+# Override build_py to be able to execute a command
+class my_build_py(build_py):
+    def run(self):
+        if not exists("{}/src/configuration_files".format(os.getcwd())):
+            os.mkdir("{}/src/configuration_files".format(os.getcwd()))
+
+
+
 
 setup(
     name='HappyMail',
@@ -6,4 +18,5 @@ setup(
     packages=['src',],
     license='MIT License',
     long_description=open('README.md', mode='r').read(),
+    cmdclass={'build_py': my_build_py}
 )
