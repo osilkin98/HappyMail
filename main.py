1e93c27f4903dbe6ce21bec045ce6e1525dcaecd
Oleg Silkin <o.silkin98@gmail.com>
2018-08-30 02:12:54 -0500

    Implements main method

--- main.py
+++ main.py
@@ -15,3 +15,26 @@ def run_main_function(sleep_time=60, max_emails=200):
     """
 
     classifier = EmailClassifier(model_file=keys.models + 'trained_net.h5')
+    service = get_gmail_service()
+
+    # Initialize the first message ID
+    first_message_id = None
+
+    try:
+        while True:
+            # Have it get the email list
+            messages, first_message_id = get_email_list(service=service,
+                                                        last_message_id=first_message_id,
+                                                        max_lookback=max_emails)
+
+            # Have the program classify the messages in the inbox
+            classify_messages(negative_label='Label_4', messages=messages,
+                              classifier=classifier, threshold=0.25)
+
+            print("first message id: " + first_message_id)
+            # Then have it sleep like a ferret
+            sleep(sleep_time)
+
+    except (KeyboardInterrupt, EOFError) as err:
+        print("Exiting program")
+
