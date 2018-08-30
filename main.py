720016ace5ce00e0b3ceb69079d92ee98b04e480
Oleg Silkin <o.silkin98@gmail.com>
2018-08-30 02:52:41 -0500

    Finalizes main function and fixes formatting bugs and adds verbosity

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.classifier import EmailClassifier
 from src.email_filter import classify_messages, get_email_list
 from src.scraper import get_gmail_service
 import src.configuration_files.keys as keys
+from colorama import Fore
 from time import sleep
 
 
@@ -14,7 +15,7 @@ def run_main_function(sleep_time=60, max_emails=200):
     :return: Nothing
     """
 
-    classifier = EmailClassifier(model_file=keys.models + 'trained_net.h5')
+    classifier = EmailClassifier(model_file=keys.models + '/trained_net.h5')
     service = get_gmail_service()
 
     # Initialize the first message ID
@@ -28,13 +29,18 @@ def run_main_function(sleep_time=60, max_emails=200):
                                                         max_lookback=max_emails)
 
             # Have the program classify the messages in the inbox
-            classify_messages(negative_label='Label_4', messages=messages,
+            # 'Label_5' is the negative label
+            classify_messages(negative_label='Label_5', messages=messages,
                               classifier=classifier, threshold=0.25)
 
             print("first message id: " + first_message_id)
+            print("Sleeping like a ferret for " + Fore.GREEN + str(sleep_time) + Fore.RESET + " seconds")
             # Then have it sleep like a ferret
             sleep(sleep_time)
 
     except (KeyboardInterrupt, EOFError) as err:
         print("Exiting program")
 
+
+if __name__ == '__main__':
+    run_main_function(max_emails=10)
